85fe0f01c69b681193be062b1e90dd6d9304d81f
Justin Chu <justinchuby@users.noreply.github.com>
2025-06-07 18:46:47 -0700

    demo 3

--- resources/attention.py
+++ resources/attention.py
@@ -2,26 +2,23 @@ import torch
 
 
 class AttentionModel(torch.nn.Module):
-    def __init__(self, input_dim, hidden_dim, output_dim):
+    def __init__(self, input_dim, num_heads):
         super().__init__()
         self.attention = torch.nn.MultiheadAttention(
-            embed_dim=input_dim, num_heads=1, batch_first=True
+            embed_dim=input_dim, num_heads=num_heads, batch_first=True
         )
-        self.output = torch.nn.Linear(hidden_dim, output_dim)
 
     def forward(self, q, k, v):
-        # x shape: (seq_len, batch_size, input_dim)
+        # x shape: (batch_size, seq_len, input_dim)
         attn_output, _ = self.attention(q, k, v, need_weights=False)
-        # attn_output shape: (seq_len, batch_size, input_dim)
-        output = self.output(attn_output)
-        # output shape: (seq_len, batch_size, output_dim)
-        return output
+        # attn_output shape: (batch_size, seq_len, input_dim)
+        return attn_output
 
 
-model = AttentionModel(input_dim=64, hidden_dim=64, output_dim=10).eval()
-q = torch.randn(10, 2, 64)  # (seq_len, batch_size, input_dim)
-k = torch.randn(10, 2, 64)  # (seq_len, batch_size, input_dim)
-v = torch.randn(10, 2, 64)  # (seq_len, batch_size, input_dim)
+model = AttentionModel(input_dim=64, num_heads=4).eval()
+q = torch.randn(2, 10, 64)  # (batch_size, seq_len, input_dim)
+k = torch.randn(2, 10, 64)  # (batch_size, seq_len, input_dim)
+v = torch.randn(2, 10, 64)  # (batch_size, seq_len, input_dim)
 inputs = (q, k, v)
 
 output_tensor = model(q, k, v)
@@ -29,4 +26,11 @@ output_tensor = model(q, k, v)
 onnx_program = torch.onnx.export(
     model, inputs, opset_version=23, dynamo=True, report=True
 )
-onnx_program.save("attention_model.onnx")
+onnx_program.save("attention_model_23.onnx")
+
+import onnxscript.rewriter.ort_fusions
+
+(ort_model, counts) = onnxscript.rewriter.ort_fusions.optimize_for_ort(onnx_program.model, debug=True)
+print(ort_model)
+print(counts)
+onnx_program.save("attention_model_optimized_23.onnx")
