a7e0ad4332b0812eff78c288d4057de827c1f8ba
Justin Chu <justinchuby@users.noreply.github.com>
2025-06-07 17:08:01 -0700

    attention

--- resources/attention.py
+++ resources/attention.py
@@ -2,21 +2,17 @@ import torch
 
 
 class AttentionModel(torch.nn.Module):
-    def __init__(self, input_dim, hidden_dim):
+    def __init__(self, input_dim, hidden_dim, output_dim):
         super().__init__()
-        self.q_weights = torch.nn.Parameter(torch.randn(input_dim, hidden_dim))
-        self.k_weights = torch.nn.Parameter(torch.randn(input_dim, hidden_dim))
-        self.v_weights = torch.nn.Parameter(torch.randn(input_dim, hidden_dim))
+        self.attention = torch.nn.MultiheadAttention(embed_dim=input_dim, num_heads=1, batch_first=True)
+        self.output = torch.nn.Linear(hidden_dim, output_dim)
 
     def forward(self, q, k, v):
-        q = torch.matmul(q, self.q_weights)
-        k = torch.matmul(k, self.k_weights)
-        v = torch.matmul(v, self.v_weights)
-
-        attn_scores = torch.matmul(q, k.transpose(-2, -1)) / (k.size(-1) ** 0.5)
-        attn_weights = torch.nn.functional.softmax(attn_scores, dim=-1)
-
-        output = torch.matmul(attn_weights, v)
+        # x shape: (seq_len, batch_size, input_dim)
+        attn_output, _ = self.attention(q, k, v, need_weights=False)
+        # attn_output shape: (seq_len, batch_size, input_dim)
+        output = self.output(attn_output)
+        # output shape: (seq_len, batch_size, output_dim)
         return output
 
 
@@ -26,8 +22,9 @@ k = torch.randn(10, 2, 64)  # (seq_len, batch_size, input_dim)
 v = torch.randn(10, 2, 64)  # (seq_len, batch_size, input_dim)
 inputs = (q, k, v)
 
-output_tensor = model(*inputs)
+output_tensor = model(q, k, v)
 
 onnx_program = torch.onnx.export(
     model, inputs, opset_version=23, dynamo=True, report=True
 )
+onnx_program.save("attention_model.onnx")
